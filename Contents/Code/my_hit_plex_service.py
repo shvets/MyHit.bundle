bc3803aff7b3be38f880b560745d3e86dc5579e3
Alexander Shvets <alexander.shvets@gmail.com>
2016-05-12 21:36:04 -0400

    reorganize code

--- Contents/Code/my_hit_plex_service.py
+++ Contents/Code/my_hit_plex_service.py
@@ -1,9 +1,16 @@
 from my_hit_service import MyHitService
 
-from my_hit_storage import MyHitStorage
+from my_hit_plex_storage import MyHitPlexStorage
 
 class MyHitPlexService(MyHitService):
     def __init__(self):
         storage_name = Core.storage.abs_path(Core.storage.join_path(Core.bundle_path, 'Contents', 'myhit.storage'))
 
-        self.queue = MyHitStorage(storage_name)
+        self.queue = MyHitPlexStorage(storage_name)
+
+        self.queue.register_simple_type('movie')
+        self.queue.register_simple_type('track')
+        self.queue.register_simple_type('serie')
+        self.queue.register_simple_type('selection')
+        self.queue.register_simple_type('tracks')
+        self.queue.register_simple_type('soundtrack')
--- Contents/Code/my_hit_plex_storage.py
+++ Contents/Code/my_hit_plex_storage.py
@@ -1,18 +1,11 @@
 import constants
 from plex_storage import PlexStorage
 
-class MyHitStorage(PlexStorage):
+class MyHitPlexStorage(PlexStorage):
     def __init__(self, file_name):
         PlexStorage.__init__(self, Core.storage, file_name)
 
         self.load()
-
-        self.register_simple_type('movie')
-        self.register_simple_type('track')
-        self.register_simple_type('serie')
-        self.register_simple_type('selection')
-        self.register_simple_type('tracks')
-        self.register_simple_type('soundtrack')
 
     def append_controls(self, oc, handler, media_info):
         bookmark = self.find(media_info)
